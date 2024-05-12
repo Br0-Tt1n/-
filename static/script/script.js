163379da4d4589c7622d8e8f384f5f195aae22e6
_Br0_ <dervroman12@mail.ru>
2024-05-12 12:10:59 +0300

    index_page
    главная страница сайта на которой расположены все нужные элементы

---
+++ static/script/script.js
@@ -0,0 +1,15 @@
+
+
+// function openMenu() {
+//     var open_menu = document.getElementById('open_menu');
+//     var close_menu = document.getElementById('close_menu');
+//     open_menu.classList.add("show_menu")
+//     close_menu.classList.add("show_menu")
+// }
+
+// function closeMenu() {
+//     var open_menu = document.getElementById('open_menu');
+//     var close_menu = document.getElementById('close_menu');
+//     open_menu.classList.remove("show_menu")
+//     close_menu.classList.remove("show_menu")
+// }
